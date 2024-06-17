38963e592e8b2968d04f72bbfeb4fc4e5e685c3b
vsl700 <51147745+vsl700@users.noreply.github.com>
2024-06-17 23:04:51 +0300

    Basic Sudoku board (no rules, just typing numbers)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { createContext, useContext, useState } from 'react';
 import './App.css';
 
+const SudokuContext = createContext(-1);
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [ selected, setSelected ] = useState(-1);
+
+    const onFieldClicked = (id: number) => {
+        setSelected(id);
+    }
+
+    return (
+        <div className='App'>
+            {/* If you click outside of all Sudoku fields, it should unselect them */}
+            <div className='Sudoku-bg' onClick={() => setSelected(-1)}/>
+            {/* Sudoku Context for selected field */}
+            <SudokuContext.Provider value={selected}>
+                {/* The actual Sudoku grid */}
+                <div className='Sudoku-grid'>
+                    <Square3x3 startId={0} onFieldClicked={onFieldClicked} />
+                    <Square3x3 startId={9} onFieldClicked={onFieldClicked} />
+                    <Square3x3 startId={18} onFieldClicked={onFieldClicked} />
+                    <Square3x3 startId={27} onFieldClicked={onFieldClicked} />
+                    <Square3x3 startId={36} onFieldClicked={onFieldClicked} />
+                    <Square3x3 startId={45} onFieldClicked={onFieldClicked} />
+                    <Square3x3 startId={54} onFieldClicked={onFieldClicked} />
+                    <Square3x3 startId={63} onFieldClicked={onFieldClicked} />
+                    <Square3x3 startId={72} onFieldClicked={onFieldClicked} />
+                </div>
+            </SudokuContext.Provider>
+        </div>
+    );
+}
+
+const Square3x3 = (props: {startId: number, onFieldClicked: (fieldId: number) => void}) => {
+    let startId = props.startId;
+
+    return (
+        <div className='Sudoku-3x3-square'>
+            <SquareField id={startId++} onClicked={props.onFieldClicked} />
+            <SquareField id={startId++} onClicked={props.onFieldClicked} />
+            <SquareField id={startId++} onClicked={props.onFieldClicked} />
+            <SquareField id={startId++} onClicked={props.onFieldClicked} />
+            <SquareField id={startId++} onClicked={props.onFieldClicked} />
+            <SquareField id={startId++} onClicked={props.onFieldClicked} />
+            <SquareField id={startId++} onClicked={props.onFieldClicked} />
+            <SquareField id={startId++} onClicked={props.onFieldClicked} />
+            <SquareField id={startId++} onClicked={props.onFieldClicked} />
+        </div>
+    )
+}
+
+const SquareField = (props: {id: number, onClicked: (fieldId: number) => void}) => {
+    const [ number, setNumber ] = useState(0);
+    const selected = useContext(SudokuContext);
+
+    const isSelected = () => selected === props.id;
+
+    const keyPressed = (e: React.KeyboardEvent) => {
+        console.log(`Key down! (${props.id})`);
+        if(!isSelected()) return;
+
+        setNumber(parseInt(e.key));
+    }
+
+    return (
+        <div className={`Sudoku-field${isSelected() ? ' selected': ' '}`} tabIndex={0} onKeyDown={keyPressed} onClick={() => props.onClicked(props.id)}>
+            {number !== 0 ? number : ''}
+        </div>
+    )
 }
 
 export default App;
